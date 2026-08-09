17f06a12cfc1b222d989198fe8cc98e5ed862bd7
forttunes2-droid <forttunes2@gmail.com>
2026-08-09 08:36:36 +0100

    feat(property-partner): surface live finance summary

--- src/pages/PropertyOwnerDashboard.tsx
+++ src/pages/PropertyOwnerDashboard.tsx
@@ -1,4 +1,5 @@
 import PropertyInspectionRequestPanel from '@/components/PropertyInspectionRequestPanel';
+import PropertyPartnerFinancePanel from '@/components/PropertyPartnerFinancePanel';
 import LegacyPropertyOwnerDashboard from './PropertyOwnerDashboardLegacy';
 import type { Profile } from '@/types';
 
@@ -12,8 +13,9 @@ interface Props {
 export default function PropertyOwnerDashboard(props: Props) {
   return (
     <div className="min-h-screen bg-[#0A0A0F]">
-      <div className="relative z-50 px-4 pt-4 lg:px-8">
+      <div className="relative z-50 px-4 pt-4 lg:px-8 space-y-4">
         <PropertyInspectionRequestPanel profile={props.profile} />
+        <PropertyPartnerFinancePanel profile={props.profile} />
       </div>
       <LegacyPropertyOwnerDashboard {...props} />
     </div>
