e52af76d80c04457aed8107ce1d8d15d38ad2aac
forttunes2-droid <forttunes2@gmail.com>
2026-08-17 23:29:36 +0100

    chore: record production database hardening migration

---
+++ supabase/migrations/20260817231500_revoke_anonymous_security_definer_execution.sql
@@ -0,0 +1,7 @@
+DO $$
+DECLARE fn record;
+BEGIN
+  FOR fn IN SELECT p.oid::regprocedure AS signature FROM pg_proc p JOIN pg_namespace n ON n.oid=p.pronamespace WHERE n.nspname='public' AND p.prosecdef AND has_function_privilege('anon',p.oid,'EXECUTE')
+  LOOP EXECUTE format('REVOKE EXECUTE ON FUNCTION %s FROM anon',fn.signature); END LOOP;
+END
+$$;
