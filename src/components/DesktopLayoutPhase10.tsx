f8257a6ae72b2ce43a4ee12acd8fc51b3356d667
forttunes2-droid <forttunes2@gmail.com>
2026-08-17 23:29:18 +0100

    fix: mount the global in-app notification surface

--- src/components/DesktopLayoutPhase10.tsx
+++ src/components/DesktopLayoutPhase10.tsx
@@ -1,7 +1,8 @@
 import type { ComponentProps } from 'react';
 import DesktopLayoutUnified from '@/components/DesktopLayoutUnified';
 import PrivateCallCenter from '@/components/PrivateCallCenterStable';
+import { Toaster } from 'sonner';
 
 export default function DesktopLayoutPhase10(props:ComponentProps<typeof DesktopLayoutUnified>){
-  return <><DesktopLayoutUnified {...props}/><PrivateCallCenter/></>;
+  return <><Toaster position="top-center" richColors closeButton/><DesktopLayoutUnified {...props}/><PrivateCallCenter/></>;
 }
